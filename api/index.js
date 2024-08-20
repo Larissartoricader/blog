a63d92d9c734add9d47aa902edc23280c7d0edc4
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-08-20 21:38:53 +0200

    deploy done

--- api/index.js
+++ api/index.js
@@ -1,19 +1,3 @@
-// const express = require("express");
-// const app = express();
-
-// app.set("view engine", "ejs");
-
-// // Rota principal
-// app.get("/", (req, res) => {
-//   res.render("api/index", { title: "Hello, Vercel!" });
-// });
-
-// // Inicia o servidor na porta 3000
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Servidor rodando na porta ${PORT}`);
-// });
-
 import express from "express";
 const port = process.env.PORT || 3000;
 
