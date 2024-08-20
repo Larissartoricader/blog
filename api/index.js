0387a53df1c38a1e68c330309ed36b5272d1fb54
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-08-20 21:36:16 +0200

    port

--- api/index.js
+++ api/index.js
@@ -15,11 +15,12 @@
 // });
 
 import express from "express";
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(port, () => console.log("Server ready on port 3000."));
 
 export default app;
